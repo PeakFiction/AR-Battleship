7169f05a42278fe9b9c4c23d6084dbdc9851c429
PeakFiction <112671939+PeakFiction@users.noreply.github.com.>
2026-03-30 22:04:18 +1000

    Add placeholder scene navigation to all scenes

---
+++ AR-Battleship/Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene("1TitleScreen");
+    }
+
+    public void LoadLobbyScreen()
+    {
+        SceneManager.LoadScene("2LobbyScreen");
+    }
+
+    public void LoadLobbySetup()
+    {
+        SceneManager.LoadScene("3LobbySetup");
+    }
+
+    public void LoadJoinLobby()
+    {
+        SceneManager.LoadScene("4JoinLobby");
+    }
+
+    public void LoadGameplay()
+    {
+        SceneManager.LoadScene("5Gameplay");
+    }
+}
