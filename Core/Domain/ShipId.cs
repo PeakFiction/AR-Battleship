75f4998b8403a3da82bab82aff2872ecabe34b57
Clayton Nagle <c.nagle744@gmail.com>
2026-04-04 13:34:27 +1000

    feat: Added global unique id for ships

---
+++ Core/Domain/ShipId.cs
@@ -0,0 +1,17 @@
+using System;
+
+public readonly struct ShipId(Guid value) : IEquatable<ShipId>
+{
+    private readonly Guid _value = value;
+
+    public static ShipId New() => new(Guid.NewGuid());
+
+    public bool Equals(ShipId other) => _value.Equals(other._value);
+
+    public override bool Equals(object obj) =>
+        obj is ShipId other && Equals(other);
+
+    public override int GetHashCode() => _value.GetHashCode();
+
+    public override string ToString() => _value.ToString();
+}
