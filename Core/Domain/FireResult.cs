7aaffa2f2e9944fe5e42a6e2ea74db63fb0d6374
Clayton Nagle <c.nagle744@gmail.com>
2026-04-07 14:20:21 +1000

    refactor: FireResult factories

--- Core/Domain/FireResult.cs
+++ Core/Domain/FireResult.cs
@@ -1,12 +1,12 @@
-    public record FireResult(
-        Coordinate Coordinate,
-        ShotResult Result,
-        ShipId? ShipId
-    )
-    {
-        public bool IsHit => Result is ShotResult.Hit or ShotResult.Sunk;
-        public bool IsSunk => Result == ShotResult.Sunk;
-        public string Message =>
+public record FireResult
+{
+    public Coordinate Coordinate { get; init; }
+    public ShotResult Result { get; init; }
+    public ShipId? ShipId { get; init; }
+
+    public bool IsHit => Result is ShotResult.Hit or ShotResult.Sunk;
+    public bool IsSunk => Result == ShotResult.Sunk;
+    public string Message =>
         Result switch
         {
             ShotResult.Miss => "Miss",
@@ -14,4 +14,20 @@
             ShotResult.Sunk => "Ship sunk!",
             _ => throw new InvalidOperationException("Invalid shot result")
         };
+
+    private FireResult(Coordinate coordinate, ShotResult result, ShipId? shipId)
+    {
+        Coordinate = coordinate;
+        Result = result;
+        ShipId = shipId;
     }
+
+    public static FireResult Miss(Coordinate coord) =>
+        new(coord, ShotResult.Miss, null);
+
+    public static FireResult Hit(Coordinate coord, ShipId shipId) =>
+        new(coord, ShotResult.Hit, shipId);
+
+    public static FireResult Sunk(Coordinate coord, ShipId shipId) =>
+        new(coord, ShotResult.Sunk, shipId);
+}
