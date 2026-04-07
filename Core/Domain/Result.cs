7a49bc4997520371f3a9c2eb4e253cee40b1250a
Clayton Nagle <c.nagle744@gmail.com>
2026-04-07 14:15:30 +1000

    fix: implemented Result logic

---
+++ Core/Domain/Result.cs
@@ -0,0 +1,23 @@
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public string? Error { get; }
+    public T? Value { get; }
+
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+        Error = null;
+    }
+
+    private Result(string error)
+    {
+        IsSuccess = false;
+        Error = error;
+        Value = default;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(string error) => new(error);
+}
