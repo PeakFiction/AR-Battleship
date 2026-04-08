f4730ca4adb333f25c6031f228980165c2caca25
Clayton Nagle <c.nagle744@gmail.com>
2026-04-08 15:32:06 +1000

    fix: nullability error

--- Core/Domain/ShipId.cs
+++ Core/Domain/ShipId.cs
@@ -8,7 +8,7 @@ public readonly struct ShipId(Guid value) : IEquatable<ShipId>
 
     public bool Equals(ShipId other) => _value.Equals(other._value);
 
-    public override bool Equals(object obj) =>
+    public override bool Equals(object? obj) =>
         obj is ShipId other && Equals(other);
 
     public override int GetHashCode() => _value.GetHashCode();
